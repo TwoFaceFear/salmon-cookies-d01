a1a65b43669981c9008ff3a650a9b81974fda457
Benjamin Cahill <benjaminacahill@gmail.com>
2017-04-10 18:53:44 -0700

    added number gen to each object

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var randomNumber;
 var minCustomers;
 var maxCustomers;
 var avgCookies;
-var totalCookies
+var totalCookies;
 var cookiesPerHour;
 var firstPike;
 var seaTac;
@@ -18,32 +18,49 @@ function randomNumber(minCustomers, maxCustomers) {
   return Math.floor(Math.random() * (maxCustomers - minCustomers) + minCustomers);
 }
 
+
+
 firstPike = {
-  minCustomers = 23;
-  maxCustomers = 65;
-  avgCookies = 6.3;
+  minCustomers: 23;
+  maxCustomers: 65;
+  avgCookies: 6.3;
+  cookiesPerHour: function() {
+    return randomNumber(this.minCustomers, this.maxCustomers);
+  }
 }
 
 seaTac = {
   minCustomers: 3;
   maxCustomers: 24;
   avgCookies: 1.2;
+  cookiesPerHour: function() {
+    return randomNumber(this.minCustomers, this.maxCustomers);
+  }
 }
 
 seaCenter = {
   minCustomers: 11;
   maxCustomers: 38;
   avgCookies: 3.7;
+  cookiesPerHour: function() {
+    return randomNumber(this.minCustomers, this.maxCustomers);
+  }
 }
 
 capHill = {
   minCustomers: 20
   maxCustomers: 38
   avgCookies: 2.3;
+  cookiesPerHour: function() {
+    return randomNumber(this.minCustomers, this.maxCustomers);
+  }
 }
 
 alki = {
   minCustomers: 2;
   maxCustomers: 16;
   avgCookies:2.6;
+  cookiesPerHour: function() {
+    return randomNumber(this.minCustomers, this.maxCustomers);
+  }
 }
