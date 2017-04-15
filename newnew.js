b39fe2501eb9bcaced1daec52746bd9670dd9104
Benjamin Cahill <benjaminacahill@gmail.com>
2017-04-15 12:48:49 -0700

    i cant get the submit button to work but everything else is woking normally

--- newnew.js
+++ newnew.js
@@ -67,28 +67,49 @@ function getTable() {
 
 var table = document.getElementById('table');
 
-getTable();
-var pike = new Branch('1st and Pike', 23, 65,6.3);
-pike.rng;
-pike.addInfo();
-console.log('pike',pike);
+function main() {
+  getTable();
+  var pike = new Branch('1st and Pike', 23, 65,6.3);
+  pike.rng;
+  pike.addInfo();
+  console.log('pike',pike);
 
-var seaTac = new Branch('SeaTac', 3, 24, 1.2);
-seaTac.rng;
-seaTac.addInfo();
-console.log('seaTac',seaTac);
+  var seaTac = new Branch('SeaTac', 3, 24, 1.2);
+  seaTac.rng;
+  seaTac.addInfo();
+  console.log('seaTac',seaTac);
 
-var seaCenter = new Branch('Seattle Center', 11, 38, 3.7);
-seaCenter.rng;
-seaCenter.addInfo();
-console.log('seaCenter',seaCenter);
+  var seaCenter = new Branch('Seattle Center', 11, 38, 3.7);
+  seaCenter.rng;
+  seaCenter.addInfo();
+  console.log('seaCenter',seaCenter);
 
-var capHill = new Branch('Capital Hill', 20, 38, 2.3);
-capHill.rng;
-capHill.addInfo();
-console.log('capHill',capHill);
+  var capHill = new Branch('Capital Hill', 20, 38, 2.3);
+  capHill.rng;
+  capHill.addInfo();
+  console.log('capHill',capHill);
 
-var alki = new Branch('Alki', 2, 16, 4.6);
-alki.rng;
-alki.addInfo();
-console.log('alki',alki);
+  var alki = new Branch('Alki', 2, 16, 4.6);
+  alki.rng;
+  alki.addInfo();
+  console.log('alki',alki);
+}
+
+function storeCreate(event) {
+  event.preventDefault();
+
+  var form = event.target;
+
+  var branchTitle = form.branchTitle.value;
+  var minCust = parseInt(form.minCust.value);
+  var maxCust = parseInt(form.maxCust.value);
+  var avgCookies = parseInt(form.avgCookies.value);
+
+  var branch = new Branch(branchTitle, minCust, maxCust, avgCookies);
+  document.getElementById('table').appendChild(branch.addInfo());
+
+  form.reset();
+}
+document.getElementById('store-create').addEventListner('submit', storeCreate);
+
+main();
